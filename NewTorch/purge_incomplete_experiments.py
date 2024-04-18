0c3dc37bc9f51500e850d9548cfb67f19b033a1c
Ashwin Kumar <ashwinkumar@wustl.edu>
2024-04-17 20:23:46 -0400

    Update purge_incomplete_experiments.py
    Made the purging general, to handle all runs of one env together

--- NewTorch/purge_incomplete_experiments.py
+++ NewTorch/purge_incomplete_experiments.py
@@ -4,14 +4,16 @@
 # Given a top level directory containing multiple experiments for different beta values, this script opens all of them, looks through all bootstraps (folders) and removes any that do not contain a results.csv file
 
 import os
-expt_dir = "logs/WarmStart/Simple/split_diff/Joint/"
+base_dir = "logs/WarmStart/Simple/split_diff/"
 
-for beta in os.listdir(expt_dir):
-    beta_dir = os.path.join(expt_dir, beta)
-    for bootstrap_id in os.listdir(beta_dir):
-        bootstrap_dir = os.path.join(beta_dir, bootstrap_id)
-        if not os.path.exists(os.path.join(bootstrap_dir, "results.csv")):
-            print(f"Removing {bootstrap_dir}")
-            # recursively delete all files in the directory
-            print(f"rm -rf {bootstrap_dir}")
-            os.system(f"rm -rf {bootstrap_dir}")
+for expts in os.listdir(base_dir):
+	expt_dir = os.path.join(base_dir, expts)
+	for beta in os.listdir(expt_dir):
+		beta_dir = os.path.join(expt_dir, beta)
+		for bootstrap_id in os.listdir(beta_dir):
+			bootstrap_dir = os.path.join(beta_dir, bootstrap_id)
+			if not os.path.exists(os.path.join(bootstrap_dir, "results.csv")):
+				print(f"Removing {bootstrap_dir}")
+				# recursively delete all files in the directory
+				print(f"rm -rf {bootstrap_dir}")
+				os.system(f"rm -rf {bootstrap_dir}")
